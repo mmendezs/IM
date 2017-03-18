a350e08de0ea500a456d62a773f451aa13972dc1
mmendezs <mariano.mendez@esic.edu>
2017-03-18 11:47:22 +0100

    tratando de limpiar los datos

--- inicio.R
+++ inicio.R
@@ -1,1 +1,9 @@
 a <- 'hola mundo'
+library(dplyr)
+
+summary(survey)
+
+survey %>% select(starts_with('P')) %>% 
+  mutate(replace(0, NA))
+
+survey <- mutate_all(survey,funs(replace(., starts_with('P') ==0, NA)))
