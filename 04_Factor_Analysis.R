cc6ec13e091da2aa1b9eac881d9088e09e474b1d
mmendezs <mariano.mendez@esic.edu>
2017-05-07 11:24:49 +0200

    factor base

--- 04_Factor_Analysis.R
+++ 04_Factor_Analysis.R
@@ -252,142 +252,23 @@ ggplot(data = mercadona_principal_scores_rot, aes(RC1, RC2)) +
 
 
 
+## Explicación de la rotación
+x_sin <- c(-4, -2, 1, 3, 2)+6
+y_sin <- c(4,0,1,-0.5,-2.5)+6
+sin_rotar <- data.frame(x = x_sin, y = y_sin, rot='sin rotar', modelo = c('A', 'B', 'C', 'D', 'E'))
+x_rot <- c(4,3.8,1,-2,-6)+6
+y_rot <- c(-1,1.4,0.25,-1.3,0.7)+5
+rotado <- data.frame(x = x_rot, y = y_rot, rot='rotado', modelo = c('E', 'D', 'C', 'B', 'A'))
+datos <- bind_rows(sin_rotar, rotado)
+datos$rot <- factor(datos$rot, levels = c('sin rotar', 'rotado'))
 
-
-
-
-
-
-
-
-
-
-## psych & Base R
-survey_Base_prin <- princomp(select(survey, starts_with('P')),4, scores = TRUE)
-survey_Base_pr <- prcomp(select(survey, starts_with('P')),4, scale = FALSE)
-screeplot(survey_Base_pr) # Variance
-screeplot(survey_Base_prin) # std deviation
-
-psych::fa.sort(survey_principal)
-
- 
-
-nfactors(select(survey, starts_with('P')), n= 20, rotate = 'varimax')
-
-
-
-## FactoMineR
-survey_principal <- principal(select(survey, starts_with('P')),4 ,rotate = 'varimax',
-                              scores = TRUE)
-a <- cor(select(survey, starts_with('P')))
-VSS.plot(a)
-survey_principal$scores
-# Para extraer varianza
-a <- print(survey_principal)
-round(a$Vaccounted,2)
-cor(survey_principal$scores) 
-## FactoMineR
-# PCA All
-survey_pca <- PCA(select(survey, starts_with('P')))
-print(survey_pca)
-prcomp(select(survey, starts_with('P')), rotate = TRUE)
-(survey_pca$eig)
-fviz_screeplot(survey_pca)
-a <- survey_pca$var$coord
-b <-t(apply(survey_pca$var$coord, 1, function(x) {x/sqrt(survey_pca$eig[,1])}))
-head(sweep(survey_pca$var$coord, 2, sqrt(survey_pca$eig[,1]),'/'))
-
-# PCA Día
-dia_pca <- PCA(na.omit(select(dia, starts_with('P'))))
-print(dia_pca)
-(dia_pca$eig)
-fviz_screeplot(dia_pca)
-dia_pca$var$coord
-
-# PCA Carrefour
-carrefour_pca <- PCA(na.omit(select(carrefour, starts_with('P'))))
-print(carrefour_pca)
-(carrefour_pca$eig)
-fviz_screeplot(carrefour_pca)
-carrefour_pca$var$coord
-
-# PCA Mercadona
-mercadona_pca <- PCA(na.omit(select(mercadona, starts_with('P'))))
-print(mercadona_pca)
-(mercadona_pca$eig)
-fviz_screeplot(mercadona_pca)
-mercadona_pca$var$coord
-
-# Quality Cos2
-head(mercadona_pca$var$cos2)
-
-
-ggplot(survey, aes(Ph1_Satisfaccion_Global, RC4_Compra_Media,
-                   color = Establecimiento)) +
-  geom_point(shape=1) +
-  scale_colour_hue(l=50) + # Use a slightly darker palette than normal
-  geom_smooth(method=lm,   # Add linear regression lines
-              se=FALSE,    # Don't add shaded confidence region
-              fullrange=TRUE) # Extend regression lines
-
-ggplot(mercadona, aes(Ph1_Satisfaccion_Global, RC4_Compra_Media)) +
-  geom_point(shape=1) +
-  scale_colour_hue(l=50) + # Use a slightly darker palette than normal
-  geom_smooth(method=lm,   # Add linear regression lines
-              se=FALSE,    # Don't add shaded confidence region
-              fullrange=TRUE) # Extend regression lines
-
-
-# Rotación
-seed(123)
-sim_data <-  as.data.frame(MASS::mvrnorm(
-  n=100, mu=c(0, 0), Sigma=matrix(c(1, 0.8, 0.8, 1), nrow=2), empirical=TRUE))
-
-names(sim_data) <- c('x', 'y')
-
-line1 <- data.frame(x = seq(from = -2, to = 2, by = 1/100),
-                    y = seq(from = -2, to = 2, by = 1/100))
-line2 <- data.frame(x = seq(from = -1, to = 1, by = 1/100),
-                    y = -seq(from = -1, to = 1, by = 1/100))
-
-ggplot(sim_data, aes(x, y)) +
-  geom_point() +
-  geom_line(data = line1, aes(x, y)) +
-  geom_line(data = line2, aes(x, y))
-
-rotate <- function(df, degree) {
-  dfr <- df
-  degree <- pi * degree / 180
-  l <- sqrt(df$start1^2 + df$start2^2)
-  teta <- atan(df$start2 / df$start1)
-  dfr$start1 <- round(l * cos(teta - degree))
-  dfr$start2 <- round(l * sin(teta - degree))
-  return(dfr)
-}
-
-sim_data1 <- rotate(sim_data, -90)
-
-sim_data1 <- sim_data
-degree <- 45
-degree <- pi * degree / 180
-l <- sqrt(sim_data1$x^2 + sim_data$y^2)
-teta <- atan(sim_data$y / sim_data1$x)
-sim_data1$x <- l * cos(teta - degree)
-sim_data1$y <- l * sin(teta - degree)
-
-ggplot(sim_data1, aes(x, y)) +
-  geom_point() +
-  geom_line(data = line1, aes(x, y)) +
-  geom_line(data = line2, aes(x, y))
-
-factor.rotate(sim_data, 45, col1=sim_data$x, col2=sim_data$y, plot=TRUE)
-
-data(Harman23.cor)
-f2 <- fa(Harman23.cor$cov,2,rotate="none")
-op <- par(mfrow=c(1,2))
-cluster.plot(f2,xlim=c(-1,1),ylim=c(-1,1),title="Unrotated ")
-f2r <- factor.rotate(f2,-33,plot=TRUE,xlim=c(-1,1),ylim=c(-1,1),title="rotated -33 degrees")
-op <- par(mfrow=c(1,1))
-
+ggplot(datos, aes(x,y)) +
+  geom_hline(yintercept = 0, colour = "gray70") +
+  geom_vline(xintercept = 0, colour = "gray70") +
+  geom_point(fill = NA, alpha = 0) +
+  geom_smooth(method=lm, se=FALSE) +
+  facet_grid(~ rot) +
+  geom_text(aes(label = modelo)) +
+  labs(x='deportividad', y = 'comodidad')
 
 
